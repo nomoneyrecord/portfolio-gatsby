d6054bda20f3e19b7a50e1696bad7c5d20986d9b
Gary H <3rdeye.ghj@gmail.com>
2023-05-15 12:29:31 -0700

    Removed blog link till first article is completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,8 @@ export default function IndexPage() {
       <Seo title="Gary Hughes Jr" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
+        {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        <AboutSection sectionId="about" heading="About Portfolio Minimal" /> */}
         <AboutSection sectionId="about" heading="About Me" />
         <InterestsSection sectionId="details" heading="Skills" />
         <ProjectsSection sectionId="features" heading="Projects" />
